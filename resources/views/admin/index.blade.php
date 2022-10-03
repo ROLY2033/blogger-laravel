b5f0a5311095f4e6c943e2cf3e8f58725a785401
ROLY ANIBAL QUELLCA <mdoom2022@gmail.com>
2022-10-02 19:28:26 -0500

    agrengado el adminlte mas su uso

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,21 @@
-<h1>prueba de administrador</h1>
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+    {{-- :addams user password: 123456789 --}}
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+

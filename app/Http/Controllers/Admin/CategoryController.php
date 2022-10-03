1968060a2925764aa55442b1ea337cdd0f792d75
ROLY ANIBAL QUELLCA <mdoom2022@gmail.com>
2022-10-02 21:12:55 -0500

    agrando funcionalidad al boton de crear lista categoria

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -40,6 +40,7 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         //
+        return $request->all();
     }
 
     /**
--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -19,14 +19,28 @@
 
                     <div class="form-group">
                         {!! Form::label('slug','slug',) !!}
-                        {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'ingrese el slug de la categoria']) !!}
+                        {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'ingrese el slug de la categoria', 'readonly']) !!}
                     </div>
 
 
                     {!! Form::submit('Crear categoria', ['class' => 'btn btn-primary']) !!}
-                    
+
                 {!! Form::close() !!}
             </div>
         </div>
 @stop
 
+@section('js')
+    <script src="{{ asset('vendor/jQuery-Plugin-stringToSlug-1.3/jquery.stringToSlug.min.js')}}"></script>
+
+    <script>
+        $(document).ready( function() {
+        $("#name").stringToSlug({
+            setEvents: 'keyup keydown blur',
+            getPut: '#slug',
+            space: '-'
+        });
+        });
+    </script>
+
+@endsection
